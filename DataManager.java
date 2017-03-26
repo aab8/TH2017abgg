bb4dc8cb477dee09ba833d6f99ce7f064a8bad8c
ggs12 <gretarsaemundsson@gmail.com>
2017-03-26 13:52:13 +0000

    TestScanner eytt og gert að fallinu scanFile í DataManager.  Gert main-fall í DataManager sem prófaði scanFile og það virkaði.

--- DataManager.java
+++ DataManager.java
@@ -1,5 +1,49 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class DataManager {
-
+	public static void main(String[] args) throws FileNotFoundException {
+		String fileName = "src/flugtest.csv";
+		List<List<String>> lines = scanFile(fileName);
+		int lineNo = 1;
+	    for(List<String> line: lines) {
+	      int columnNo = 1;
+	      for (String value: line) {
+	        System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
+	        columnNo++;
+	      }
+	      lineNo++;
+	    }
+	}
+public static List<List<String>> scanFile(String fileName) {
+	
+	File file = new File(fileName);
+    // this gives you a 2-dimensional array of strings
+    List<List<String>> lines = new ArrayList<>();
+    Scanner inputStream;
+   
+    try
+    {
+      inputStream = new Scanner(file);
+     
+      while(inputStream.hasNext()){
+        String line = inputStream.next();
+        String[] values = line.split(",");
+       
+        // this adds the currently parsed line to the 2-dimensional string array
+        lines.add(Arrays.asList(values));
+      }
+      inputStream.close();
+    }
+   
+    catch (FileNotFoundException e)
+    {
+      e.printStackTrace();
+    }
+    return lines;
+}
 }
