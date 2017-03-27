9c07c9ab06e3ef21985360246c4eea1d4a7d141b
helgimararthursson <helgimararthursson@vr2t-dw007.rhi.hi.is>
2017-03-27 17:32:26 +0000

    Þetta er ég, Gunnar Arthúr, að reyna að tengjast GITHUB

--- SearchAndBook.java
+++ SearchAndBook.java
@@ -2,22 +2,22 @@
 import java.util.ArrayList;
 
 public class SearchAndBook {
-private ArrayList<Flight> flightList;
+	private ArrayList<Flight> flightList;
 
-public ArrayList<Flight> customSort(ArrayList<Flight> flightList, String sortBy, boolean order) {
-	
-	return flightList;
-}
+	public ArrayList<Flight> customSort(ArrayList<Flight> flightList, String sortBy, boolean order) {
 
-public ArrayList<Flight> searchFlight(String whereTo, String whereFrom, String time){
-	
-	return flightList;
-}
+		return flightList;
+	}
 
-public void printCurrentResults(ArrayList<Flight> flightList){
-	System.out.println("FLUGLISTI");
-}
+	public ArrayList<Flight> searchFlight(String whereTo, String whereFrom, String time){
+
+		return flightList;
+	}
+
+	public void printCurrentResults(ArrayList<Flight> flightList){
+		System.out.println("FLUGLISTI");
+	}
 
 
-// Virkar að breyta firi Gunna? <– þetta er próf
+	// Virkar að breyta firi Gunna? <– þetta er próf
 }
