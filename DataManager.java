711d4338449d5b64d0fd55eae3dbe98d693c4577
gunnararthur <gunnararthur@172.20.10.3>
2017-03-28 02:47:34 +0000

    Smá prentleikfimi
    Ekki kíkja á klukkuna

--- DataManager.java
+++ DataManager.java
@@ -11,90 +11,90 @@ public class DataManager {
 		// String fileName = "src/flugtest.csv";
 		List<List<String>> lines = scanFile(fileName);
 		int lineNo = 1;
-	    for(List<String> line: lines) {
-	      int columnNo = 1;
-	      for (String value: line) {
-	        System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
-	        columnNo++;
-	      }
-	      lineNo++;
-	    }
+		for(List<String> line: lines) {
+			int columnNo = 1;
+			for (String value: line) {
+				System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
+				columnNo++;
+			}
+			lineNo++;
+		}
 	}
-public static List<List<String>> scanFile(String fileName) {
-	
-	File file = new File(fileName);
-    // this gives you a 2-dimensional array of strings
-    List<List<String>> lines = new ArrayList<>();
-    Scanner inputStream;
-   
-    try
-    {
-      inputStream = new Scanner(file);
-     
-      while(inputStream.hasNext()){
-        String line = inputStream.next();
-        String[] values = line.split(",");
-       
-        // this adds the currently parsed line to the 2-dimensional string array
-        lines.add(Arrays.asList(values));
-      }
-      inputStream.close();
-    }
-   
-    catch (FileNotFoundException e)
-    {
-      e.printStackTrace();
-    }
-    return lines;
-}
-public static ArrayList<Flight> crunchFile(String fileName){
-	//Lesum inn skrana
-	List<List<String>> flightFile = scanFile(fileName);
-	//Fjoldi fluga
-	int numFlights = flightFile.size();
-	//Foruthluta
-	ArrayList<Flight> flightList = new ArrayList<Flight>(numFlights);
-	//Herna aetti ad koma einhvers konar lykkja
-	for (int i = 0;i < numFlights;i++){
-		Flight flight = new Flight();
-		//Fara i gegnum hverja linu � skranni fyrir sig og gefa flight eiginleika.
-		//Fylla svo upp � flightList i lok hverrar itrunar
-		
-		//whereTo - Afangastadur
-		Place whereFrom = new Place();
-		String whereFromName = flightFile.get(i).get(0);
-		whereFrom.setName(whereFromName);
-		flight.setWhereFrom(whereFrom);
-		
-		//whereFrom - Brottfararstadur
-		Place whereTo = new Place();
-		String whereToName = flightFile.get(i).get(1);
-		whereTo.setName(whereToName);
-		flight.setWhereTo(whereTo);
-		
-		//price - Verd
-		double price = Double.parseDouble(flightFile.get(i).get(5));
-		flight.setPrice(price);
-		
-		
-		//time - Timi dags
-		flight.setTime(flightFile.get(i).get(3));
-		
-		//date - Dagsetning
-		flight.setDate(flightFile.get(i).get(2));
-		
-		//duration - Lengd flugs i klst
-		double duration = Double.parseDouble(flightFile.get(i).get(4));
-		flight.setDuration(duration);
-		
-		/*
+	public static List<List<String>> scanFile(String fileName) {
+
+		File file = new File(fileName);
+		// this gives you a 2-dimensional array of strings
+		List<List<String>> lines = new ArrayList<>();
+		Scanner inputStream;
+
+		try
+		{
+			inputStream = new Scanner(file);
+
+			while(inputStream.hasNext()){
+				String line = inputStream.next();
+				String[] values = line.split(",");
+
+				// this adds the currently parsed line to the 2-dimensional string array
+				lines.add(Arrays.asList(values));
+			}
+			inputStream.close();
+		}
+
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		return lines;
+	}
+	public static ArrayList<Flight> crunchFile(String fileName){
+		//Lesum inn skrana
+		List<List<String>> flightFile = scanFile(fileName);
+		//Fjoldi fluga
+		int numFlights = flightFile.size();
+		//Foruthluta
+		ArrayList<Flight> flightList = new ArrayList<Flight>(numFlights);
+		//Herna aetti ad koma einhvers konar lykkja
+		for (int i = 0;i < numFlights;i++){
+			Flight flight = new Flight();
+			//Fara i gegnum hverja linu � skranni fyrir sig og gefa flight eiginleika.
+			//Fylla svo upp � flightList i lok hverrar itrunar
+
+			//whereTo - Afangastadur
+			Place whereFrom = new Place();
+			String whereFromName = flightFile.get(i).get(0);
+			whereFrom.setName(whereFromName);
+			flight.setWhereFrom(whereFrom);
+
+			//whereFrom - Brottfararstadur
+			Place whereTo = new Place();
+			String whereToName = flightFile.get(i).get(1);
+			whereTo.setName(whereToName);
+			flight.setWhereTo(whereTo);
+
+			//price - Verd
+			double price = Double.parseDouble(flightFile.get(i).get(5));
+			flight.setPrice(price);
+
+
+			//time - Timi dags
+			flight.setTime(flightFile.get(i).get(3));
+
+			//date - Dagsetning
+			flight.setDate(flightFile.get(i).get(2));
+
+			//duration - Lengd flugs i klst
+			double duration = Double.parseDouble(flightFile.get(i).get(4));
+			flight.setDuration(duration);
+
+			/*
 		private String airline;
 		private String flightNumber;
 		private int numSeatsLeft;
-		*/
-		flightList.add(flight);
+			 */
+			flightList.add(flight);
+		}
+
+		return flightList;
 	}
-	
-	return flightList;
-}
 }
--- SearchAndBook.java
+++ SearchAndBook.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
-// ÞETTA KOMMENT ER BARA GITHUB PRUFA
+import java.util.Collections;
+
 public class SearchAndBook {
 	public static void main(String[] args) {
 		String fileName = "flugtest.csv";
@@ -11,15 +12,19 @@ public class SearchAndBook {
 		String hvenaer = "15.05.17";
 
 		ArrayList<Flight> test = searchFlight(hvert, hvadan, hvenaer);
-
-		System.out.println(test.size());
-
+		
+		/*
 		System.out.println("Leitaði að flugi frá " + hvadan + " til " + hvert);
 		System.out.println("Fann flug frá " + test.get(0).getWhereFrom().getName() + " til " + test.get(0).getWhereTo().getName() );
 		if(test.size()>1) {
 			System.out.println("Fann líka flug frá " + test.get(1).getWhereFrom().getName()  + " til " + test.get(1).getWhereTo().getName() );
 		}
-		
+		*/
+		ArrayList<Flight> flightList = DataManager.crunchFile(fileName);
+		System.out.println("Fann þessi flug:\n");
+		printCurrentResults(test);
+		System.out.println("\nAf öllum þessum!\n");
+		printCurrentResults(flightList);
 	}
 
 
@@ -54,8 +59,14 @@ public class SearchAndBook {
 		return foundFlight;
 	}
 
-	public void printCurrentResults(ArrayList<Flight> flightList){
-		System.out.println("FLUGLISTI");
+	public static void printCurrentResults(ArrayList<Flight> flightList){
+		System.out.println("Frá\tTil\tDags\t\tVerð");
+		for(int i = 0; i < flightList.size(); i++) {
+			System.out.print(flightList.get(i).getWhereTo().getName() + "\t");
+			System.out.print(flightList.get(i).getWhereFrom().getName() + "\t");
+			System.out.print(flightList.get(i).getDate() + "\t");
+			System.out.println(flightList.get(i).getPrice());
+		}
 	}
 
 }
